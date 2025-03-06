128328dea005ae9813d662f8f6101242ef908089
oliveirabrunoa <oliveirabrunoa@gmail.com>
2025-03-06 13:34:29 -0300

    Configuração inicial da feature de filtro por mercado

--- project_anac/views.py
+++ project_anac/views.py
@@ -1,8 +1,29 @@
-from flask import Blueprint, render_template
-from flask import request
+from flask import Blueprint, render_template, request, redirect
+from . import models
+from db import db
 
 project_anac = Blueprint('project_anac', __name__)
 
-@project_anac.route('/')
+@project_anac.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    voos = models.Voos.query.all()
+    return render_template('index.html', voos=voos)
+
+@project_anac.route('/filtro_mercado', methods=['GET', 'POST'])
+def filtro_mercado():
+    lista_mercados = models.Voos.query.with_entities(models.Voos.mercado).distinct().all()
+    mercados_unicos = [mercado[0] for mercado in lista_mercados]
+
+    if request.method=='POST':
+        mercado_selecionado = request.form['mercado']
+        print(mercado_selecionado)
+        voos = models.Voos.query.filter_by(mercado=mercado_selecionado).all()
+        return render_template('mercados.html', voos=voos, mercados=mercados_unicos)
+    
+    return render_template('mercados.html', mercados=mercados_unicos)
+
+
+
+
+
+    
