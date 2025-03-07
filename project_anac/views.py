5abbce23a215e4ee0e3380cf0e4693ad2ca0ef46
oliveirabrunoa <oliveirabrunoa@gmail.com>
2025-03-06 22:39:58 -0300

    Correções e ajustes na página de cadastro e login

--- project_anac/views.py
+++ project_anac/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from . import models
 from db import db
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from io import BytesIO
 import base64
 from flask_login import login_required
@@ -56,20 +57,19 @@ def filtro_data():
 @project_anac.route('/grafico_rpk', methods=['GET', 'POST'])
 @login_required
 def grafico_rpk():
-    lista_meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]  # Meses de 1 a 12
-    lista_anos = models.Voos.query.with_entities(models.Voos.ano).distinct().all()  # Anos distintos no banco
+    lista_meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    lista_anos = models.Voos.query.with_entities(models.Voos.ano).distinct().all()
     anos_unicos = [ano[0] for ano in lista_anos]
     lista_mercados = models.Voos.query.with_entities(models.Voos.mercado).distinct().all()
     mercados_unicos = [mercado[0] for mercado in lista_mercados]
 
     if request.method == 'POST':
-        mes_inicio = int(request.form['mes_inicio'])  # Convertendo para inteiro
-        ano_inicio = int(request.form['ano_inicio'])  # Convertendo para inteiro
-        mes_final = int(request.form['mes_final'])  # Convertendo para inteiro
-        ano_final = int(request.form['ano_final'])  # Convertendo para inteiro
-        mercado = request.form['mercado']  # Mercado selecionado
+        mes_inicio = int(request.form['mes_inicio'])
+        ano_inicio = int(request.form['ano_inicio'])
+        mes_final = int(request.form['mes_final'])
+        ano_final = int(request.form['ano_final'])
+        mercado = request.form['mercado']
 
-        # Filtrando voos no intervalo de datas
         voos = models.Voos.query.filter(
             (models.Voos.ano > ano_inicio) | 
             ((models.Voos.ano == ano_inicio) & (models.Voos.mes >= mes_inicio)),
@@ -78,40 +78,24 @@ def grafico_rpk():
             models.Voos.mercado == mercado
         ).all()
 
-        # Organizando os dados para o gráfico
-        datas = []
-        rpk_values = []
+        if not voos:
+            return render_template('grafico_rpk.html', mensagem='Nenhum dado encontrado.', meses=lista_meses, anos=anos_unicos, mercados=mercados_unicos)
 
-        for voo in voos:
-            # Formatação do mês/ano para o eixo X
-            data_formatada = f'{voo.mes}/{voo.ano}'
-            datas.append(data_formatada)
-            rpk_values.append(voo.rpk)
+        datas = [f'{voo.mes}/{voo.ano}' for voo in voos]
+        rpk_values = [voo.rpk for voo in voos]
 
-        # Criando o gráfico com Matplotlib
-        fig, ax = plt.subplots(figsize=(13, 6))
-        ax.plot(datas, rpk_values, marker='o', linestyle='-', color='b')
+        fig = go.Figure(data=go.Scatter(x=datas, y=rpk_values, mode='lines+markers', line=dict(color='blue')))
+        fig.update_layout(title=f'Gráfico de RPK para o Mercado {mercado}', xaxis_title='Data', yaxis_title='RPK', xaxis_tickangle=-45)
 
-        # Adicionando título e rótulos aos eixos
-        ax.set_title(f'Gráfico de RPK para o Mercado {mercado}')
-        ax.set_xlabel('Data')
-        ax.set_ylabel('RPK')
-
-        # Rotacionando os rótulos do eixo X para melhor visualização
-        plt.xticks(rotation=45)
-        # Adicionando os valores de RPK no gráfico com mais casas decimais
-        for i, v in enumerate(rpk_values):
-            ax.text(i, v, f'{v:.2f}', ha='center', va='bottom', fontsize=10)  # Ajuste para exibir até 2 casas decimais
-        # Convertendo o gráfico para base64 para ser exibido na página HTML
         img_bytes = BytesIO()
-        fig.savefig(img_bytes, format='png')
+        fig.write_image(img_bytes, format='png')
         img_bytes.seek(0)
         img_base64 = base64.b64encode(img_bytes.read()).decode('utf-8')
 
-        # Retornar o template com o gráfico gerado
         return render_template('grafico_rpk.html', img_base64=img_base64, meses=lista_meses, anos=anos_unicos, mercados=mercados_unicos)
 
-    return render_template('grafico_rpk.html', meses=lista_meses, anos=anos_unicos,mercados=mercados_unicos)
+    return render_template('grafico_rpk.html', meses=lista_meses, anos=anos_unicos, mercados=mercados_unicos)
+
 
 
 
